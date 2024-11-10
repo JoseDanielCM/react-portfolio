6d5e5fb27c6e2dc2402c439f06df9e2b8d2bbf31
JoseDanielCM <jodacarmon@gmail.com>
2024-11-09 21:12:19 -0500

    feat: :sparkles: Add navBar dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
+import FloatingNavDemo from './components/FloatingNavDemo'
+
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FloatingNavDemo/>
       <MainContent />
       <Footer />
     </div>
